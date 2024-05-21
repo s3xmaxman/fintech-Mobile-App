d6b07081ab8cd2db9194e9cb340313dc5ab62191
s3xmaxman <badside3919@gmail.com>
2024-05-21 17:17:32 +0900

    progress signUp

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,11 @@ import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 import { Link, Stack, useRouter, useSegments } from 'expo-router';
+import Colors from '@/constants/Colors';
+import { TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { StatusBar } from 'expo-status-bar';
 
 
 
@@ -16,7 +21,8 @@ export {
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
-export default function RootLayout() {
+const InitialLayout = () => {
+  const router = useRouter();
   const [loaded, error] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
     ...FontAwesome.font,
@@ -37,15 +43,33 @@ export default function RootLayout() {
     return null;
   }
 
-  return <RootLayoutNav />;
-}
-
-function RootLayoutNav() {
-
-
   return (
-        <Stack>
-          <Stack.Screen name='index' options={{ headerShown: false }}  />
-        </Stack>
+    <Stack>
+      <Stack.Screen name='index' options={{ headerShown: false }}  />
+      <Stack.Screen name='signup' options={{ 
+        title: '',
+        headerBackTitle: '',
+        headerShadowVisible: false,
+        headerStyle: { backgroundColor: Colors.background },
+        headerLeft: () => (
+          <TouchableOpacity onPress={router.back}>
+            <Ionicons name="arrow-back" size={34} color={Colors.dark} />
+          </TouchableOpacity>
+        ),
+      }}
+      />
+  </Stack>
   );
 }
+
+const RootLayoutNav = () => {
+  
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+       <StatusBar style="light" />
+      <InitialLayout />
+    </GestureHandlerRootView>
+  );
+}
+
+export default RootLayoutNav
--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,10 @@
 
-import { View, Text, StyleSheet } from 'react-native';
-import { Video, ResizeMode } from 'expo-av';
+import Colors from '@/constants/Colors';
+import { defaultStyles } from '@/constants/Styles';
 import { useAssets } from 'expo-asset';
+import { ResizeMode, Video } from 'expo-av';
+import { Link } from 'expo-router';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 const index = () => {
     const [assets] = useAssets([require('../assets/videos/intro.mp4')]);
@@ -24,6 +27,27 @@ const index = () => {
       />
       <View style={{ marginTop: 80, padding: 20 }}>
         <Text style={styles.header}>Ready to change the way you money?</Text>
+      </View>
+
+      <View style={styles.buttons}>
+        <Link 
+          href={'/login'} 
+          style={[defaultStyles.pillButton, { flex: 1, backgroundColor: Colors.dark}]} 
+          asChild
+        >
+            <TouchableOpacity>
+                <Text style={{ color: 'white', fontSize: 22, fontWeight: '500' }}>Log in</Text>
+            </TouchableOpacity>
+        </Link>
+        <Link
+          href={'/signup'}
+          style={[defaultStyles.pillButton, { flex: 1, backgroundColor: '#fff' }]}
+          asChild
+        >
+          <TouchableOpacity>
+            <Text style={{ fontSize: 22, fontWeight: '500' }}>Sign up</Text>
+          </TouchableOpacity>
+        </Link>
       </View>
     </View>
   );
@@ -53,5 +77,6 @@ const styles = StyleSheet.create({
       marginBottom: 60,
       paddingHorizontal: 20,
     },
-  });
+});
+
 export default index
